23a03e790b641ab13a53f0de9678fe110c4200e1
Billie Mead <bmead@mjhlifesciences.com>
2024-01-19 19:13:19 -0500

    wrap div in new section div for background image

--- wp-content/themes/conall-child/pharvaris-hcp-scripts.js
+++ wp-content/themes/conall-child/pharvaris-hcp-scripts.js
@@ -1,1 +1,15 @@
 // pharvaris-deflate-hcp JS
+
+
+//Select the elements you want inside
+const divs = document.querySelectorAll("#home-tradeoff-row, #hae-banner-section, #hae-facts-section");
+
+// create the div to wrap your elements
+const wrapper = document.createElement("div");
+wrapper.classList.add('section-background');
+
+// add it to the DOM
+divs[0].before(wrapper);
+
+// insert the elements into the newly created div
+divs.forEach(div => wrapper.append(div));
