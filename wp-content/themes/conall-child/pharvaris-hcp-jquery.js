8a21941ccd4769994ec1368b13d6d2fb9e5af20f
Billie Mead <bmead@mjhlifesciences.com>
2024-02-10 04:33:11 -0500

    fix div change js

--- wp-content/themes/conall-child/pharvaris-hcp-jquery.js
+++ wp-content/themes/conall-child/pharvaris-hcp-jquery.js
@@ -16,25 +16,60 @@ jQuery(document).ready(function ($) {
         $('.ult-tooltipster-base').tooltip('hide')
     })
 }); */
-
-var bgArray = [
-    '/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg',
-    '/wp-content/themes/conall-child/images/living-backg-2-1920x1200.jpg',
-    '/wp-content/themes/conall-child/images/living-backg-3-1920x1200.jpg',
-    '/wp-content/themes/conall-child/images/living-backg-4-1920x1200.jpg'
-]
-jQuery(document).ready(function ($) {
-    $('#redselect').on('change', function () {
-        value = $(this).val() - 1;
-        $('#living-hae-impacts-row').css({
-            'background-image': 'url(' + bgArray[value] + ')'
-        });
+/* jQuery(document).ready(function ($) {
+    $(window).resize(function () {
+        $('#test').html($(window).width());
+        if ($(window).width() > 768) {
+            alert();
+        }
     });
-});
+}); */
+//Function to the css rule
 jQuery(document).ready(function ($) {
-    $(document).on('click', '.pv-red-select', function () {
-        $(this).parent().toggleClass('focused');
-    })
+    $(window).resize(function () {
+        $('#living-hero-row').html($(window).width());
+        if ($(window).width() > 1024) {
+            var bgArray = [
+                '/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-2-1920x1200.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-3-1920x1200.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-4-1920x1200.jpg'
+            ]
+            $('#redselect').on('change', function () {
+                value = $(this).val() - 1;
+                $('#living-hae-impacts-row').css({
+                    'background-image': 'url(' + bgArray[value] + ')'
+                });
+            });
+        } else if ($(window).width() > 768) {
+            var bgArray = [
+                '/wp-content/themes/conall-child/images/living-backg-1-1024x1475.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-2-1024x1475.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-3-1024x1475.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-4-1024x1475.jpg'
+            ]
+            $('#redselect').on('change', function () {
+                value = $(this).val() - 1;
+                $('#living-hae-impacts-row').css({
+                    'background-image': 'url(' + bgArray[value] + ')'
+                });
+            });
+
+        } else if ($(window).width() > 480) {
+            var bgArray = [
+                '/wp-content/themes/conall-child/images/living-backg-1-576x1280.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-2-576x1280.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-3-576x1280.jpg',
+                '/wp-content/themes/conall-child/images/living-backg-4-576x1280.jpg'
+            ]
+            $('#redselect').on('change', function () {
+                value = $(this).val() - 1;
+                $('#living-hae-impacts-row').css({
+                    'background-image': 'url(' + bgArray[value] + ')'
+                });
+            });
+        }
+    });
 });
 
 /* jQuery(document).ready(function ($) {
--- wp-content/themes/conall-child/pharvaris-hcp-scripts.js
+++ wp-content/themes/conall-child/pharvaris-hcp-scripts.js
@@ -1,48 +1,28 @@
 // pharvaris-deflate-hcp JS
 
-var changeBgImage = function () {
-
-    var idBackgnd = document.getElementById("redselect");
-    /* var contain1 = document.getElementById('listswap-container-1'); */
-
-    idBackgnd.style.backgroundImage = "url('/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg')";
-
-    if (document.getElementById("living-hae-impacts-row").value === "occupational") {
-        idBackgnd.style.backgroundImage = "url('/wp-content/themes/conall-child/images/living-backg-2-1920x1200.jpg')";
-    } else if (document.getElementById("living-hae-impacts-row").value === "social") {
-        idBackgnd.style.backgroundImage = "url('/wp-content/themes/conall-child/images/living-backg-3-1920x1200.jpg')";
-    } else if (document.getElementById("living-hae-impacts-row").value === "familial") {
-        idBackgnd.style.backgroundImage = "url('/wp-content/themes/conall-child/images/living-backg-4-1920x1200.jpg')";
-    } else {
-        idBackgnd.style.backgroundImage = "url('/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg')";
-    }
-    idBackgnd.style.backgroundColor = document.getElementById("living-hae-impacts-row").value;
-
-};
-
-/* function showDiv(select) {
-    if (select.value == "emotional") {
+function showDiv(select) {
+    if (select.value == 1) {
         document.getElementById('listswap-container-1').style.display = "block";
     }
     else {
         document.getElementById('listswap-container-1').style.display = "none";
     }
-    if (select.value == "occupational") {
+    if (select.value == 2) {
         document.getElementById('listswap-container-2').style.display = "block";
     }
     else {
         document.getElementById('listswap-container-2').style.display = "none";
     }
-    if (select.value == "social") {
+    if (select.value == 3) {
         document.getElementById('listswap-container-3').style.display = "block";
     }
     else {
         document.getElementById('listswap-container-3').style.display = "none";
     }
-    if (select.value == "familial") {
+    if (select.value == 4) {
         document.getElementById('listswap-container-4').style.display = "block";
     }
     else {
         document.getElementById('listswap-container-4').style.display = "none";
     }
-} */
+}
