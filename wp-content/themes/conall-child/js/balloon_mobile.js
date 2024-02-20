c84729389939da32174cbd885cfa4620cbafcba5
Billie Mead <bmead@mjhlifesciences.com>
2024-02-20 15:54:25 -0500

    fix mobiel balloon ani sizing

--- wp-content/themes/conall-child/js/balloon_mobile.js
+++ wp-content/themes/conall-child/js/balloon_mobile.js
@@ -1,4 +1,23 @@
 const riveCanvas = document.getElementById("rive-canvas");
+// Fill the canvas, cropping Rive if necessary
+let layout = new rive.Layout({
+    fit: rive.Fit.Cover,
+});
+
+// Fit to the width and align to the top of the canvas
+layout = new rive.Layout({
+    fit: rive.Fit.FitWidth,
+    alignment: rive.Alignment.TopCenter,
+});
+
+// Constrain the Rive content to (minX, minY), (maxX, maxY) in the canvas
+layout = new rive.Layout({
+    fit: rive.Fit.Contain,
+    minX: 50,
+    minY: 50,
+    maxX: 100,
+    maxY: 100,
+});
 
 const balloon = new rive.Rive({
     src: "/wp-content/themes/conall-child/animations/pharvaris_balloon_animation_-_feb_20_2024_v3.riv",
@@ -6,9 +25,11 @@ const balloon = new rive.Rive({
     stateMachines: "balloon_sm",
     autoplay: true,
     artboard: "mobile-576x1130",
-    fit: rive.Fit.Contain,
-    // layout: layout,
+    // fit: rive.Fit.Contain,
+    layout: layout,
     onLoad: () => {
         balloon.resizeDrawingSurfaceToCanvas();
     },
 });
+
+balloon.layout = new rive.Layout({ fit: rive.Fit.Fill });
