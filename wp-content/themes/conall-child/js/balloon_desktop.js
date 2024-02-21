9bb9ca06d0ab3c055ac95fdbd530557c8afae09f
Billie Mead <bmead@mjhlifesciences.com>
2024-02-20 21:27:10 -0500

    balloon ani v5

--- wp-content/themes/conall-child/js/balloon_desktop.js
+++ wp-content/themes/conall-child/js/balloon_desktop.js
@@ -21,7 +21,7 @@ layout = new rive.Layout({
 });
 
 const balloon = new rive.Rive({
-    src: "/wp-content/themes/conall-child/animations/pharvaris_balloon_animation_-_feb_20_2024_v3.riv",
+    src: "/wp-content/themes/conall-child/animations/pharvaris_balloon_animation_-_feb_20_2024_v5.riv",
     canvas: riveCanvas,
     stateMachines: "balloon_sm",
     autoplay: true,
