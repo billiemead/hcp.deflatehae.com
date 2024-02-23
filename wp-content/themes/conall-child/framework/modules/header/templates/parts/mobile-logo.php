5f65183394d3f193c9ea35389d3762c5afacf465
Billie Mead <bmead@mjhlifesciences.com>
2024-02-23 08:24:23 -0500

    fix mobile home link

--- wp-content/themes/conall-child/framework/modules/header/templates/parts/mobile-logo.php
+++ wp-content/themes/conall-child/framework/modules/header/templates/parts/mobile-logo.php
@@ -1,7 +1,7 @@
 <?php do_action('conall_edge_before_mobile_logo'); ?>
 
 <div class="edgtf-mobile-logo-wrapper hcp-logo">
-    <a href="https://hcp.deflatehae.com/" <?php conall_edge_inline_style($logo_styles); ?>>
+    <a href="/" <?php conall_edge_inline_style($logo_styles); ?>>
         <img class="edgtf-normal-mobile-logo" src="/wp-content/themes/conall-child/images/mobile-logo-140x36.svg" alt="<?php esc_attr_e('mobile logo','conall'); ?>"/>
     </a>
 </div>
