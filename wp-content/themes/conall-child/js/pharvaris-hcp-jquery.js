c72dda9b8ddcafc3a5ee5f29831aa3ea807046e0
Billie Mead <bmead@mjhlifesciences.com>
2024-02-27 12:39:21 -0500

    fix backgroudn swap

--- wp-content/themes/conall-child/js/pharvaris-hcp-jquery.js
+++ wp-content/themes/conall-child/js/pharvaris-hcp-jquery.js
@@ -1,70 +1,69 @@
 // pharvaris-deflate jQuery
 
-//Function to the css rule
 jQuery(document).ready(function ($) {
-    var bgArray = [
-        '/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg',
-        '/wp-content/themes/conall-child/images/living-backg-2-1920x1200.jpg',
-        '/wp-content/themes/conall-child/images/living-backg-3-1920x1200.jpg',
-        '/wp-content/themes/conall-child/images/living-backg-4-1920x1200.jpg'
-    ]
-    $('#redselect').on('change', function () {
-        value = $(this).val() - 1;
-        $('#living-hae-impacts-row').css({
-            'background-image': 'url(' + bgArray[value] + ')'
-        });
-    });
-    $(window).resize(function () {
-        $('#breakpoint-row').html($(window).width());
-        if ($(window).width() > 1025) {
-            var bgArray = [
-                '/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-2-1920x1200.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-3-1920x1200.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-4-1920x1200.jpg'
-            ]
-            $('#redselect').on('change', function () {
-                value = $(this).val() - 1;
+    $(window).load(function () {
+        function check() {
+            if ($(window).width() > 1024) {
                 $('#living-hae-impacts-row').css({
-                    'background-image': 'url(' + bgArray[value] + ')'
+                    'background-image': 'url(/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg)'
                 });
-            });
-        } else if ($(window).width() > 768) {
-            var bgArray = [
-                '/wp-content/themes/conall-child/images/living-backg-1-1024x1475.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-2-1024x1475.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-3-1024x1475.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-4-1024x1475.jpg'
-            ]
-            $('#redselect').on('change', function () {
-                value = $(this).val() - 1;
+                var bgArray = [
+                    '/wp-content/themes/conall-child/images/living-backg-1-1920x1200.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-2-1920x1200.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-3-1920x1200.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-4-1920x1200.jpg'
+                ]
+                $('#redselect').on('change', function () {
+                    value = $(this).val() - 1;
+                    $('#living-hae-impacts-row').css({
+                        'background-image': 'url(' + bgArray[value] + ')'
+                    });
+                });
+            } else if ($(window).width() > 768) {
                 $('#living-hae-impacts-row').css({
-                    'background-image': 'url(' + bgArray[value] + ')'
+                    'background-image': 'url(/wp-content/themes/conall-child/images/living-backg-1-1024x1475.jpg)'
                 });
-            });
 
-        } else if ($(window).width() > 480) {
-            var bgArray = [
-                '/wp-content/themes/conall-child/images/living-backg-1-576x1280.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-2-576x1280.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-3-576x1280.jpg',
-                '/wp-content/themes/conall-child/images/living-backg-4-576x1280.jpg'
-            ]
-            $('#redselect').on('change', function () {
-                value = $(this).val() - 1;
-                $('#living-hae-impacts-row').css({
-                    'background-image': 'url(' + bgArray[value] + ')'
+                var bgArray = [
+                    '/wp-content/themes/conall-child/images/living-backg-1-1024x1475.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-2-1024x1475.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-3-1024x1475.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-4-1024x1475.jpg'
+                ]
+                $('#redselect').on('change', function () {
+                    value = $(this).val() - 1;
+                    $('#living-hae-impacts-row').css({
+                        'background-image': 'url(' + bgArray[value] + ')'
+                    });
                 });
-            });
+            } else if ($(window).width() > 576) {
+                $('#living-hae-impacts-row').css({
+                    'background-image': 'url(/wp-content/themes/conall-child/images/living-backg-1-576x1280.jpg)'
+                });
+                var bgArray = [
+                    '/wp-content/themes/conall-child/images/living-backg-1-576x1280.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-2-576x1280.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-3-576x1280.jpg',
+                    '/wp-content/themes/conall-child/images/living-backg-4-576x1280.jpg'
+                ]
+                $('#redselect').on('change', function () {
+                    value = $(this).val() - 1;
+                    $('#living-hae-impacts-row').css({
+                        'background-image': 'url(' + bgArray[value] + ')'
+                    });
+                });
+            }
         }
-    });
+        $('#breakpoint-row').html($(window).width());
+        check(); // first-time check
+
+        $(window).resize(function () {
+            $('#breakpoint-row').html($(window).width());
+            check();
+        });
+    })
 });
 
-/* jQuery(document).ready(function ($) {
-    $(document).on('click', '.tooltip-close', function () {
-        $('#tipclose1').parent().hide();
-    })
-}); */
 jQuery(document).ready(function ($) {
     $('#toggler').on('click', function () {
         if ($(this).text() === "show all") {
